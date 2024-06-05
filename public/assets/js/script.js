b16e8bc7cf7177fea439e9d5dedd51018e705275
pouya <pouyamadankar@gmail.com>
2024-06-06 00:41:39 +0330

    slider created & others until to sessions

---
+++ public/assets/js/script.js
@@ -0,0 +1,33 @@
+let swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    // slidesPerGroup: 3,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+
+    // loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEL: ".swiper-button-next",
+        prevEL: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        600:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        }
+
+    }
+})
